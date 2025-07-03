2794553763f22aeb91cfe03e78c0193901a2898c
sultan-07 <sultanalih8@gmail.com>
2025-07-04 00:00:10 +0530

    minor change in backend

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ connectDB(); // Connect to the database
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-app.use(cors());
+app.use(cors("*")); // Allow all origins for CORS
 app.use(express.json());
 app.use(bodyParser.json());
 app.get('/', (req, res) => {
