6dc2322c023529625f660b42535d65ff56fef4de
sultan-07 <sultanalih8@gmail.com>
2025-07-04 22:38:42 +0530

    change origin

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ connectDB(); // Connect to the database
 const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(cors({
-  origin: ["https://docs-genapp.vercel.app"],
+  origin: ["https://docsgen-nu.vercel.app/"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
